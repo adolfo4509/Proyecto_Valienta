3be28dd3ecd6a977002fbf7c23b14493c39b94d4
adolfo4509 <calvo330906@gmail.com>
2021-11-24 10:45:42 -0500

    avances

--- proyect_valient/src/App.js
+++ proyect_valient/src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <h1>Rick and Morty</h1>
       </header>
     </div>
